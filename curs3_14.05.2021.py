bea444eb2e142449c4182355419ae43205e3a6de
Alexandra Bratu <alexandra.bratu@nxdata.ro>
2021-05-14 21:55:10 +0300

    14.05.2021

---
+++ curs3_14.05.2021.py
@@ -0,0 +1,50 @@
+lista = []
+my_list = [8, 2, -2, 6]
+my_list_string = ['aaa', 'bbb', 'ccc', ["www", "ddd", "qqq", ["rrr", "ttt"]]]
+# print(my_list_string[-1][-2])
+# print(len(my_list_string))
+# print(my_list_string[0:4:2])
+my_list_string[1] = 'ccc'
+# print(my_list_string)
+# my_list_string.append("yyy")
+# my_list_string.insert(2, "zzz")
+# my_list_string.remove("ccc")
+# my_list_string.pop(3)
+# my_list_string.clear()
+
+# print(my_list_string.index("ccc"))
+# print(my_list_string.count("ccc"))
+# my_list_string.extend(my_list)
+# print(my_list)
+# my_tuple = ()
+# my_tuple = (1, 7)
+# my_tuple_2 = (3, 4)
+# print(my_tuple + my_tuple_2)
+# my_dict = {"key_1": 1,
+#            "key_2": 2,
+#            3: 3,
+#            (2, 3): 4}
+# my_dict[4] = "2bis"
+# if 3 in my_dict:
+#     print(my_dict[3])
+# print(my_dict.get(4))
+# my_dict.update({"4": 4, 5: 5})
+# my_dict.clear()
+# my_dict.pop(3)
+# my_dict.popitem()
+# for key, value in my_dict.items():
+#     print(key, value, end=" ")
+# print(my_dict)
+# my_set = [2, 1, 2]
+# print(list(set(my_set)))
+# my_set = {1, 2, 3}
+# my_set_2 = {2, 3, 4}
+# print(my_set | my_set_2) # union
+# print(my_set & my_set_2) # intersection
+# print(my_set_2 - my_set) # difference
+# print(my_set_2 ^ my_set) # symmetric difference
+# lista = list()
+# set = set()
+# tuple = tuple()
+# my_set_2.add(5)
+# print(my_set_2)
