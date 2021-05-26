f76f394719b0b41acced82d64e6d459581d61adf
Alexandra Bratu <alexandra.bratu@nxdata.ro>
2021-05-26 20:26:17 +0300

    functii&exceptii

--- functii.py
+++ functii.py
@@ -32,4 +32,21 @@ def suma(a: int = 1, b: int = 2) -> (int, int):
 
 c, diferenta = suma(3, 3)
 # c = suma(3, 3)
-print(c)
+# print(c)
+
+
+def nume_functie(a: int, b: int, c: int = 3) -> (int, int):
+    """
+    @param a:
+    @param b:
+    @param c:
+    @return:
+    """
+    if a == 2:
+        return 2 * 2 * 2, 2 + 2 + 2
+        # d = 2 - 2 - 2
+    return a * b * c, a + b + c
+
+
+variabila1, variabila2 = nume_functie(2, b=3, c=4)
+print(variabila1, variabila2)
